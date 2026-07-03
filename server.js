a9a2adbe6a1cd8bb583752660c77ecd311a4a49e
Mounika-540 <pentanimounika1@gmail.com>
2026-07-03 12:08:05 +0530

    web server setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+PORT = 3002;
+app.use(express.static("frontend"))
+app.listen(PORT, function()
+{
+    console.log("successfully running at http://localhost:" + PORT)
+})
